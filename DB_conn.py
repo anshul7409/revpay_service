5df5dca07d1d57531bbf62f7afda056aea48d848
anshul rawat <anshulrawat541@gmail.com>
2024-06-09 15:57:19 +0530

    !

--- DB_conn.py
+++ DB_conn.py
@@ -3,9 +3,11 @@ from pymongo import MongoClient
 import os
 db_username = os.getenv('DBNAME')
 db_password = os.getenv('PASSWORD')
+db_username_bytes = db_username.encode('utf-8')
+db_password_bytes = db_password.encode('utf-8')
 class revpayDB:
     def __init__(self):
-        self.__DATABASE_URI = "mongodb+srv://{}:{}@cluster0.dqwgi9f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format(quote_plus(db_username), quote_plus(db_password))
+        self.__DATABASE_URI = "mongodb+srv://{}:{}@cluster0.dqwgi9f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format(quote_plus(db_username_bytes), quote_plus(db_password_bytes))
         self.__client =  MongoClient(self.__DATABASE_URI)
         self.__db =  self.__client['users_database']
     
